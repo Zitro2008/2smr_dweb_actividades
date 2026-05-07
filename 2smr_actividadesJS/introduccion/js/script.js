339f816e76c9976b107db33634313f8905f2f1aa
Zitro <hugoortalc08@gmail.com>
2026-05-07 13:15:51 +0200

    first commit

---
+++ 2smr_actividadesJS/introduccion/js/script.js
@@ -0,0 +1,17 @@
+console.log("mensaje desde script.js");
+
+let nombre = "Juan";
+let edad = 20;
+let aprovado = true;
+
+const pi = 3.1416;
+/** 
+if (edad >= 18 && edad < 30)  {
+    console.log(nombre + " vienes a la fiesta");
+} else {
+    console.log(nombre + " no vienes a la fiesta");
+}
+    */
+function saludo() {
+    console.log("Hola " + nombre + " de " + edad + " años");
+}
