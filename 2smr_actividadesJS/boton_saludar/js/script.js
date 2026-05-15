9e98e50adbf0b5f944f5d6578d161ebe75ccc345
Zitro <hugoortalc08@gmail.com>
2026-05-15 12:03:22 +0200

    pa casa

--- 2smr_actividadesJS/boton_saludar/js/script.js
+++ 2smr_actividadesJS/boton_saludar/js/script.js
@@ -6,14 +6,14 @@ boton.addEventListener("click", function(){
     //esta variable tiene el texto que haya en el elemento input nombre
     let contenidoCampoNombre = document.getElementById("nombre").value;
     //esta tiene la referencia al pàrrafo donde se mostrara el resultado
-    let parrafo = document.getElementById("resultado");
+    let resultado = document.getElementById("resultado");
 
     if(contenidoCampoNombre.trim() === ""){
       //aqui va el codigo que se ejecutara si el campo de texto esta vacio 
-      parrafo.textContent = "Debes escribir tu nombre";
+      resultado.textContent = "Debes escribir tu nombre";
     } else {
         //aqui va el codigo que se ejecutara si el campo de texto tiene informacion
         //muestro en el parrafo el saludo concatenado con el contenido del campo nombre
-       parrafo.textContent = "Hola " + contenidoCampoNombre.trim();
+       resultado.textContent = "Hola " + contenidoCampoNombre.trim();
     }
 });
